b71b97956f24d771e16cebd837ef327bdfde7871
maru <sorkihg@gamil.com>
2024-01-06 16:57:31 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+group = "org.example"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(18)
+}
